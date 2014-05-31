1db3fd14e1c138c3b3f5c9129d4363e95e5f7b3a
micahrlee <micahrlee@gmail.com>
2014-05-30 21:44:00 -0700

    Updates

---
+++ Racing/Assets/Scripts/FinishLine.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishLine : MonoBehaviour {
+	public bool finish = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		if(finish)
+			PrintFinish ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		finish = true;
+	}
+
+	void PrintFinish(){
+		GUILayout.BeginArea (new Rect(Screen.width/2 - 250, Screen.height * .35f, 500, 500));
+		GUILayout.Label ("You're Winner!");
+		GUILayout.EndArea ();
+	}
+}
---
+++ Racing/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using G = GameManager;
+
+public class LevelManager : MonoBehaviour {
+	public float lastCount = 0f;
+	public int CountDown = 3;
+	public bool showCountDown = true;
+	// Use this for initialization
+	void Start () {
+		G.getInstance ().PauseMovement ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(showCountDown)
+			InCountDown ();
+	}
+
+	#region CountDown
+	void InCountDown(){
+		if (CountDown > 0) {
+			UpdateCountDown ();
+			lastCount += Time.deltaTime;
+			if(lastCount >= 1){
+				CountDown--;
+				lastCount = 0f;
+			}
+		}
+		else{
+			GameObject echo = GameObject.Find ("CountDown");
+			echo.SetActive(false);
+			G.getInstance ().UnpauseMovement ();
+			showCountDown = false;
+		}
+	}
+
+	void UpdateCountDown(){
+		GameObject echo = GameObject.Find ("CountDown");
+		GUIText g = echo.GetComponent<GUIText> ();
+		if(CountDown != 0)
+			g.text = "" + CountDown;
+		else
+			g.text = "GO";
+	}
+	#endregion
+}
